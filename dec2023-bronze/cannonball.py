7470512e14410c16ee52c50e190ce76ac3e75f0e
DaPotato69 <128940918+DaPotato69@users.noreply.github.com>
2024-01-29 16:22:37 +1100

    Create cannonball.py

---
+++ dec2023-bronze/cannonball.py
@@ -0,0 +1,32 @@
+# even without all the done stuff and memorizing things it still gives the same result
+done = set()
+n, s = map(int, input().split())
+line = [list(map(int, input().split())) for i in range(n)]
+line.insert(0, [-1, 0])
+power = 1
+increasing = True
+broken = 0
+newbreak = False
+while s <= n and s > 0:
+    if newbreak == True and (power, s) in done:
+        break
+    done.add((power, s))
+    if line[s][0] == 1 and power >= line[s][1]:
+        broken += 1
+        line[s][0] = -1
+        newbreak = True
+        done = set()
+    elif line[s][0] == 0:
+        newbreak = False
+        power += line[s][1]
+        if increasing:
+            increasing = False
+        else:
+            increasing = True
+    else:
+        newbreak = False
+    if increasing:
+        s += power
+    else:
+        s -= power
+print(broken)
